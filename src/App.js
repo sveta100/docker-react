5f64e8ab0a0de98c7a1b96bd1a5550a29ff9017c
Sveta Borisenkova <sveta.borisenkova@siili.com>
2020-07-02 16:11:38 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload and this is coming from Docker! :)
         </p>
         <a
           className="App-link"
